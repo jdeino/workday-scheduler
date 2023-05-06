4cc8652c93bb644611108b24b864d2b525c0b2bb
jdeino <josh.deino91@yahoo.com>
2023-05-06 09:41:21 -0700

    updating js file

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,18 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+
+// Save Button & Local Storage Save
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".info").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
+// Grabs Current Hours
+function currHours() {
+    var currentTime = moment().hour();
+}
+
